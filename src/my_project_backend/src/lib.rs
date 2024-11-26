29c0f3f7f42bf0ad95c2f91ba7022062cefbb7cf
Michał Żyszkiewicz <u57_miczys_lbn@technischools.com>
2024-11-26 12:29:49 +0000

    Created firt version of chat

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,17 +1,22 @@
 use std::cell::RefCell;
 
 thread_local! {
-    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new())
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
 }
 
 #[ic_cdk::update]
 fn save_chat(input_chat: String) -> String {
-    CHAT.with(|chat| chat.borrow_mut().push(input_chat))
+    CHAT.with(|chat| {
+        chat.borrow_mut().push(input_chat);
+    });
+    "Chat saved".to_string() // Return a string indicating success
 }
 
 #[ic_cdk::query]
 fn get_chat() -> String {
-    CHAT.with(|chat| chat.borrow().clone())
+    CHAT.with(|chat| {
+        chat.borrow().join("\n") // Join the chat messages into a single string
+    })
 }
 
 #[ic_cdk::query]
